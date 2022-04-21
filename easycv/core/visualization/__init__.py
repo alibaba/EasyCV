9a3826f0d21bea085e5c401ac7227fe1df69040a
Cathy0908 <30484308+Cathy0908@users.noreply.github.com>
2022-04-21 20:48:58 +0800

    [Feature]: support image visualization for tensorboard and wandb (#15)
    * [Feature]: support image visualization for tensorboard and wandb

--- easycv/core/visualization/__init__.py
+++ easycv/core/visualization/__init__.py
@@ -1,4 +1,4 @@
 # Copyright (c) Alibaba, Inc. and its affiliates.
-from .image import imshow_bboxes, imshow_keypoints
+from .image import imshow_bboxes, imshow_keypoints, imshow_label
 
-__all__ = ['imshow_bboxes', 'imshow_keypoints']
+__all__ = ['imshow_bboxes', 'imshow_keypoints', 'imshow_label']
