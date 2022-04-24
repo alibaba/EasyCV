d369bffb83cd9d80ae972ac582a4d56707267bfa
yunji.cjy <yunji.cjy@alibaba-inc.com>
2022-04-24 15:00:22 +0800

    modify defaultformatbundle

--- easycv/datasets/shared/pipelines/format.py
+++ easycv/datasets/shared/pipelines/format.py
@@ -162,6 +162,11 @@ class DefaultFormatBundle(object):
     - gt_semantic_seg: (1)unsqueeze dim-0 (2)to tensor, \
                        (3)to DataContainer (stack=True)
     """
+    def __init__(self,
+                 img_to_float=True,
+                 pad_val=dict(img=0, masks=0, seg=255)):
+        self.img_to_float = img_to_float
+        self.pad_val = pad_val
 
     def __call__(self, results):
         """Call function to transform and format common fields in results.
@@ -176,21 +181,33 @@ class DefaultFormatBundle(object):
 
         if 'img' in results:
             img = results['img']
+            if self.img_to_float is True and img.dtype == np.uint8:
+                # Normally, image is of uint8 type without normalization.
+                # At this time, it needs to be forced to be converted to
+                # flot32, otherwise the model training and inference
+                # will be wrong. Only used for YOLOX currently .
+                img = img.astype(np.float32)
             # add default meta keys
             results = self._add_default_meta_keys(results)
             if len(img.shape) < 3:
                 img = np.expand_dims(img, -1)
             img = np.ascontiguousarray(img.transpose(2, 0, 1))
-            results['img'] = DC(to_tensor(img), stack=True)
+            results['img'] = DC(
+                to_tensor(img), padding_value=self.pad_val['img'], stack=True)
         for key in ['proposals', 'gt_bboxes', 'gt_bboxes_ignore', 'gt_labels']:
             if key not in results:
                 continue
             results[key] = DC(to_tensor(results[key]))
         if 'gt_masks' in results:
-            results['gt_masks'] = DC(results['gt_masks'], cpu_only=True)
+            results['gt_masks'] = DC(
+                results['gt_masks'],
+                padding_value=self.pad_val['masks'],
+                cpu_only=True)
         if 'gt_semantic_seg' in results:
             results['gt_semantic_seg'] = DC(
-                to_tensor(results['gt_semantic_seg'][None, ...]), stack=True)
+                to_tensor(results['gt_semantic_seg'][None, ...]),
+                padding_value=self.pad_val['seg'],
+                stack=True)
         return results
 
     def _add_default_meta_keys(self, results):
@@ -210,13 +227,13 @@ class DefaultFormatBundle(object):
         results.setdefault('pad_shape', img.shape)
         results.setdefault('scale_factor', 1.0)
         results.setdefault('pad', 0.0)
-        # num_channels = 1 if len(img.shape) < 3 else img.shape[2]
-        # results.setdefault(
-        #     'img_norm_cfg',
-        #     dict(
-        #         mean=np.zeros(num_channels, dtype=np.float32),
-        #         std=np.ones(num_channels, dtype=np.float32),
-        #         to_rgb=False))
+        num_channels = 1 if len(img.shape) < 3 else img.shape[2]
+        results.setdefault(
+            'img_norm_cfg',
+            dict(
+                mean=np.zeros(num_channels, dtype=np.float32),
+                std=np.ones(num_channels, dtype=np.float32),
+                to_rgb=False))
         return results
 
     def __repr__(self):
