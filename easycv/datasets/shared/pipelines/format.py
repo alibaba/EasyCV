a8ac9d5b36d9a35190eefcbc042e37183d120004
yunji.cjy <yunji.cjy@alibaba-inc.com>
2022-04-24 15:08:56 +0800

    modify aug

--- easycv/datasets/shared/pipelines/format.py
+++ easycv/datasets/shared/pipelines/format.py
@@ -64,7 +64,7 @@ class ImageToTensor(object):
             img = results[key]
             if len(img.shape) < 3:
                 img = np.expand_dims(img, -1)
-            results[key] = to_tensor(img.transpose(2, 0, 1))
+            results[key] = (to_tensor(img.transpose(2, 0, 1))).contiguous()
         return results
 
     def __repr__(self):
