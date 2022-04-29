884d8ba86fb42d65eb91a3ed88af2f690d981fe1
wenmeng.zwm <wenmeng.zwm@alibaba-inc.com>
2022-04-29 15:30:40 +0800

    update scripts

--- .scripts/linter.sh
+++ .scripts/linter.sh
@@ -1,3 +1,3 @@
-yapf -r -i easycv/ configs/ tests/ tools/ setup.py pai_jobs/easycv/resources/*.py
-isort -rc easycv/ configs/ tests/ tools/ setup.py pai_jobs/easycv/resources/*.py
-flake8 easycv/ configs/ tests/ tools/ setup.py pai_jobs/easycv/resources/*.py
+yapf -r -i easycv/ configs/ tests/ tools/ setup.py
+isort -rc easycv/ configs/ tests/ tools/ setup.py
+flake8 easycv/ configs/ tests/ tools/ setup.py
