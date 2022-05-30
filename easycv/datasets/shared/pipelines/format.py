ca857c8f44ed6bd960509fb733d2f50d5cd3eb76
yunji.cjy <yunji.cjy@alibaba-inc.com>
2022-05-30 14:59:43 +0800

    rollback

--- easycv/datasets/shared/pipelines/format.py
+++ easycv/datasets/shared/pipelines/format.py
@@ -148,11 +148,9 @@ class Collect(object):
 @PIPELINES.register_module()
 class DefaultFormatBundle(object):
     """Default formatting bundle.
-
     It simplifies the pipeline of formatting common fields, including "img",
     "proposals", "gt_bboxes", "gt_labels", "gt_masks" and "gt_semantic_seg".
     These fields are formatted as follows.
-
     - img: (1)transpose, (2)to tensor, (3)to DataContainer (stack=True)
     - proposals: (1)to tensor, (2)to DataContainer
     - gt_bboxes: (1)to tensor, (2)to DataContainer
@@ -163,18 +161,10 @@ class DefaultFormatBundle(object):
                        (3)to DataContainer (stack=True)
     """
 
-    def __init__(self,
-                 img_to_float=True,
-                 pad_val=dict(img=0, masks=0, seg=255)):
-        self.img_to_float = img_to_float
-        self.pad_val = pad_val
-
     def __call__(self, results):
         """Call function to transform and format common fields in results.
-
         Args:
             results (dict): Result dict contains the data to convert.
-
         Returns:
             dict: The result dict contains the data that is formatted with \
                 default bundle.
@@ -182,45 +172,30 @@ class DefaultFormatBundle(object):
 
         if 'img' in results:
             img = results['img']
-            if self.img_to_float is True and img.dtype == np.uint8:
-                # Normally, image is of uint8 type without normalization.
-                # At this time, it needs to be forced to be converted to
-                # flot32, otherwise the model training and inference
-                # will be wrong. Only used for YOLOX currently .
-                img = img.astype(np.float32)
             # add default meta keys
             results = self._add_default_meta_keys(results)
             if len(img.shape) < 3:
                 img = np.expand_dims(img, -1)
             img = np.ascontiguousarray(img.transpose(2, 0, 1))
-            results['img'] = DC(
-                to_tensor(img), padding_value=self.pad_val['img'], stack=True)
+            results['img'] = DC(to_tensor(img), stack=True)
         for key in ['proposals', 'gt_bboxes', 'gt_bboxes_ignore', 'gt_labels']:
             if key not in results:
                 continue
             results[key] = DC(to_tensor(results[key]))
         if 'gt_masks' in results:
-            results['gt_masks'] = DC(
-                results['gt_masks'],
-                padding_value=self.pad_val['masks'],
-                cpu_only=True)
+            results['gt_masks'] = DC(results['gt_masks'], cpu_only=True)
         if 'gt_semantic_seg' in results:
             results['gt_semantic_seg'] = DC(
-                to_tensor(results['gt_semantic_seg'][None, ...]),
-                padding_value=self.pad_val['seg'],
-                stack=True)
+                to_tensor(results['gt_semantic_seg'][None, ...]), stack=True)
         return results
 
     def _add_default_meta_keys(self, results):
         """Add default meta keys.
-
         We set default meta keys including `pad_shape`, `scale_factor` and
         `img_norm_cfg` to avoid the case where no `Resize`, `Normalize` and
         `Pad` are implemented during the whole pipeline.
-
         Args:
             results (dict): Result dict contains the data to convert.
-
         Returns:
             results (dict): Updated result dict contains the data to convert.
         """
@@ -228,13 +203,13 @@ class DefaultFormatBundle(object):
         results.setdefault('pad_shape', img.shape)
         results.setdefault('scale_factor', 1.0)
         results.setdefault('pad', 0.0)
-        num_channels = 1 if len(img.shape) < 3 else img.shape[2]
-        results.setdefault(
-            'img_norm_cfg',
-            dict(
-                mean=np.zeros(num_channels, dtype=np.float32),
-                std=np.ones(num_channels, dtype=np.float32),
-                to_rgb=False))
+        # num_channels = 1 if len(img.shape) < 3 else img.shape[2]
+        # results.setdefault(
+        #     'img_norm_cfg',
+        #     dict(
+        #         mean=np.zeros(num_channels, dtype=np.float32),
+        #         std=np.ones(num_channels, dtype=np.float32),
+        #         to_rgb=False))
         return results
 
     def __repr__(self):
