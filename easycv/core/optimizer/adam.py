6c7a0e25fc3e3e28edd1a2e98098e5d6757e271c
yunji.cjy <yunji.cjy@alibaba-inc.com>
2022-05-31 19:53:33 +0800

    compatibility torch1.8 adamw

--- easycv/core/optimizer/adam.py
+++ easycv/core/optimizer/adam.py
@@ -1,5 +1,6 @@
 # Copyright (c) Alibaba, Inc. and its affiliates.
 import math
+from distutils.version import LooseVersion
 from typing import List
 
 import torch
@@ -45,74 +46,76 @@ def adamw(params: List[Tensor], grads: List[Tensor], exp_avgs: List[Tensor],
         param.addcdiv_(exp_avg, denom, value=-step_size)
 
 
-@OPTIMIZERS.register_module(force=True)
-class AdamW(_AdamW):
-    """
-    torch1.8 bug UnboundLocalError: local variable 'beta1' referenced before assignment
-    bugfix reference: https://github.com/pytorch/pytorch/issues/55740
-    """
+if LooseVersion(torch.__version__) <= LooseVersion('1.9.0'):
 
-    @torch.no_grad()
-    def step(self, closure=None):
-        """Performs a single optimization step.
-        Args:
-            closure (callable, optional): A closure that reevaluates the model
-                and returns the loss.
+    @OPTIMIZERS.register_module(force=True)
+    class AdamW(_AdamW):
         """
-        loss = None
-        if closure is not None:
-            with torch.enable_grad():
-                loss = closure()
+        torch1.8 bug UnboundLocalError: local variable 'beta1' referenced before assignment
+        bugfix reference: https://github.com/pytorch/pytorch/issues/55740
+        """
 
-        for group in self.param_groups:
-            params_with_grad = []
-            grads = []
-            exp_avgs = []
-            exp_avg_sqs = []
-            state_sums = []
-            max_exp_avg_sqs = []
-            state_steps = []
-            amsgrad = group['amsgrad']
-            beta1, beta2 = group['betas']
+        @torch.no_grad()
+        def step(self, closure=None):
+            """Performs a single optimization step.
+            Args:
+                closure (callable, optional): A closure that reevaluates the model
+                    and returns the loss.
+            """
+            loss = None
+            if closure is not None:
+                with torch.enable_grad():
+                    loss = closure()
 
-            for p in group['params']:
-                if p.grad is None:
-                    continue
-                params_with_grad.append(p)
-                if p.grad.is_sparse:
-                    raise RuntimeError(
-                        'AdamW does not support sparse gradients')
-                grads.append(p.grad)
+            for group in self.param_groups:
+                params_with_grad = []
+                grads = []
+                exp_avgs = []
+                exp_avg_sqs = []
+                state_sums = []
+                max_exp_avg_sqs = []
+                state_steps = []
+                amsgrad = group['amsgrad']
+                beta1, beta2 = group['betas']
 
-                state = self.state[p]
+                for p in group['params']:
+                    if p.grad is None:
+                        continue
+                    params_with_grad.append(p)
+                    if p.grad.is_sparse:
+                        raise RuntimeError(
+                            'AdamW does not support sparse gradients')
+                    grads.append(p.grad)
 
-                # State initialization
-                if len(state) == 0:
-                    state['step'] = 0
-                    # Exponential moving average of gradient values
-                    state['exp_avg'] = torch.zeros_like(
-                        p, memory_format=torch.preserve_format)
-                    # Exponential moving average of squared gradient values
-                    state['exp_avg_sq'] = torch.zeros_like(
-                        p, memory_format=torch.preserve_format)
-                    if amsgrad:
-                        # Maintains max of all exp. moving avg. of sq. grad. values
-                        state['max_exp_avg_sq'] = torch.zeros_like(
+                    state = self.state[p]
+
+                    # State initialization
+                    if len(state) == 0:
+                        state['step'] = 0
+                        # Exponential moving average of gradient values
+                        state['exp_avg'] = torch.zeros_like(
                             p, memory_format=torch.preserve_format)
+                        # Exponential moving average of squared gradient values
+                        state['exp_avg_sq'] = torch.zeros_like(
+                            p, memory_format=torch.preserve_format)
+                        if amsgrad:
+                            # Maintains max of all exp. moving avg. of sq. grad. values
+                            state['max_exp_avg_sq'] = torch.zeros_like(
+                                p, memory_format=torch.preserve_format)
 
-                exp_avgs.append(state['exp_avg'])
-                exp_avg_sqs.append(state['exp_avg_sq'])
+                    exp_avgs.append(state['exp_avg'])
+                    exp_avg_sqs.append(state['exp_avg_sq'])
 
-                if amsgrad:
-                    max_exp_avg_sqs.append(state['max_exp_avg_sq'])
+                    if amsgrad:
+                        max_exp_avg_sqs.append(state['max_exp_avg_sq'])
 
-                # update the steps for each param group update
-                state['step'] += 1
-                # record the step after step update
-                state_steps.append(state['step'])
+                    # update the steps for each param group update
+                    state['step'] += 1
+                    # record the step after step update
+                    state_steps.append(state['step'])
 
-            adamw(params_with_grad, grads, exp_avgs, exp_avg_sqs,
-                  max_exp_avg_sqs, state_steps, amsgrad, beta1, beta2,
-                  group['lr'], group['weight_decay'], group['eps'])
+                adamw(params_with_grad, grads, exp_avgs, exp_avg_sqs,
+                      max_exp_avg_sqs, state_steps, amsgrad, beta1, beta2,
+                      group['lr'], group['weight_decay'], group['eps'])
 
-        return loss
+            return loss
