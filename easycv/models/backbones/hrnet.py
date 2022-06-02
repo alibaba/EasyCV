e164542939a14c593a16892c83a59edf6c860519
yunji.cjy <yunji.cjy@alibaba-inc.com>
2022-06-02 12:16:25 +0800

    bugfix hrnet pretrained

--- easycv/models/backbones/hrnet.py
+++ easycv/models/backbones/hrnet.py
@@ -8,7 +8,7 @@ from mmcv.cnn import (build_conv_layer, build_norm_layer, constant_init,
 from torch.nn.modules.batchnorm import _BatchNorm
 
 from easycv.models.registry import BACKBONES
-from ..modelzoo import resnet as model_urls
+from ..modelzoo import hrnet as model_urls
 from .resnet import BasicBlock
 
 
