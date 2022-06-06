2845a9cb7eb2ddaf3a7711e5aa519c56bfdfe259
yunji.cjy <yunji.cjy@alibaba-inc.com>
2022-06-06 10:36:59 +0800

    update

--- configs/classification/imagenet/resnet/imagenet_resnet50_jpg.py
+++ configs/classification/imagenet/resnet/imagenet_resnet50_jpg.py
@@ -3,6 +3,6 @@ _base_ = './resnet50_b32x8_100e_jpg.py'
 model = dict(
     backbone=dict(
         type='ResNet',
-        depth=50,
+        depth=51,
         out_indices=[4],  # 0: conv-1, x: stage-x
         norm_cfg=dict(type='BN')))
