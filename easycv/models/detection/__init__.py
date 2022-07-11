1fbdb30828615324be2aa7ea14681986c7f06cfc
tuofeilunhifi <18292877852@163.com>
2022-07-11 15:43:27 +0800

    revert

--- easycv/models/detection/__init__.py
+++ easycv/models/detection/__init__.py
@@ -1,3 +1,23 @@
 # Copyright (c) Alibaba, Inc. and its affiliates.
-from .detectors import *
+# Copyright (c) Alibaba, Inc. and its affiliates.
+import logging
+
+from .dab_detr import DABDETRHead, DABDetrTransformer
+from .detection import Detection
+from .detr import DETRHead, DetrTransformer
+from .fcos import FCOSHead
 from .necks import *
+
+try:
+    from .yolox.yolox import YOLOX
+except Exception as e:
+    logging.info(f'Exception: {e}')
+    logging.info(
+        'Import YOLOX failed! please check your CUDA & Pytorch Version')
+
+try:
+    from .yolox_edge.yolox_edge import YOLOX_EDGE
+except Exception as e:
+    logging.info(f'Exception: {e}')
+    logging.info(
+        'Import YOLOX_EDGE failed! please check your CUDA & Pytorch Version')
