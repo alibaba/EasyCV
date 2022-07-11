7f147ec991e91c10462829669ccc834dfdae6add
tuofeilunhifi <18292877852@163.com>
2022-07-11 19:48:22 +0800

    bugfix

--- easycv/models/detection/detectors/__init__.py
+++ easycv/models/detection/detectors/__init__.py
@@ -1,20 +1,21 @@
 # Copyright (c) Alibaba, Inc. and its affiliates.
 import logging
 
-from .dab_detr import DABDETRHead, DABDetrTransformer
-from .detection import Detection
-from .detr import DETRHead, DetrTransformer
-from .fcos import FCOSHead
+from easycv.models.detection.detectors.dab_detr import (DABDETRHead,
+                                                        DABDetrTransformer)
+from easycv.models.detection.detectors.detection import Detection
+from easycv.models.detection.detectors.detr import DETRHead, DetrTransformer
+from easycv.models.detection.detectors.fcos import FCOSHead
 
 try:
-    from .yolox.yolox import YOLOX
+    from easycv.models.detection.detectors.yolox.yolox import YOLOX
 except Exception as e:
     logging.info(f'Exception: {e}')
     logging.info(
         'Import YOLOX failed! please check your CUDA & Pytorch Version')
 
 try:
-    from .yolox_edge.yolox_edge import YOLOX_EDGE
+    from easycv.models.detection.detectors.yolox_edge.yolox_edge import YOLOX_EDGE
 except Exception as e:
     logging.info(f'Exception: {e}')
     logging.info(
