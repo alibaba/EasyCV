68573cdea5162a0bc1c83b7f038ee3ccdc837e81
zzoneee <55594658+zzoneee@users.noreply.github.com>
2022-08-05 20:24:12 +0800

    DeiT III backbone

--- easycv/models/backbones/deitiii.py
+++ easycv/models/backbones/deitiii.py
@@ -15,32 +15,34 @@ import torch.nn as nn
 from timm.models.layers import trunc_normal_
 
 
-def drop_path(x, drop_prob: float = 0., training: bool = False):
+def drop_path(x, drop_prob: float = 0., training: bool = False, scale_by_keep: bool = True):
     if drop_prob == 0. or not training:
         return x
     keep_prob = 1 - drop_prob
     shape = (x.shape[0], ) + (1, ) * (
         x.ndim - 1)  # work with diff dim tensors, not just 2D ConvNets
-    # random_tensor = keep_prob + torch.rand(
-    #     shape, dtype=x.dtype, device=x.device)
-    # random_tensor.floor_()  # binarize
+
     random_tensor = x.new_empty(shape).bernoulli_(keep_prob)
-    if keep_prob > 0.0:
+    if keep_prob > 0.0 and scale_by_keep:
         random_tensor.div_(keep_prob)
-    output = x * random_tensor
-    return output
+
+    return x * random_tensor
 
 
 class DropPath(nn.Module):
     """Drop paths (Stochastic Depth) per sample  (when applied in main path of residual blocks).
     """
 
-    def __init__(self, drop_prob: float = 0.):
+    def __init__(self, drop_prob: float = 0., scale_by_keep: bool = True):
         super(DropPath, self).__init__()
         self.drop_prob = drop_prob
+        self.scale_by_keep = scale_by_keep
 
     def forward(self, x):
         return drop_path(x, self.drop_prob, self.training)
+
+    def extra_repr(self):
+        return f'drop_prob={round(self.drop_prob,3):0.3f}'
 
 
 class Mlp(nn.Module):
@@ -232,6 +234,8 @@ class VisionTransformer(nn.Module):
                  init_scale=1e-4,
                  **kwargs):
         super().__init__()
+        self.dropout_rate = drop_rate
+        
         self.num_features = self.embed_dim = embed_dim
 
         self.patch_embed = PatchEmbed(
@@ -244,10 +248,7 @@ class VisionTransformer(nn.Module):
         self.cls_token = nn.Parameter(torch.zeros(1, 1, embed_dim))
         self.pos_embed = nn.Parameter(
             torch.zeros(1, num_patches, embed_dim))
-        self.pos_drop = nn.Dropout(p=drop_rate)
 
-        # dpr = [x.item() for x in torch.linspace(0, drop_path_rate, depth)
-        #        ]  # stochastic depth decay rule
         dpr = [drop_path_rate for i in range(depth)]
         self.blocks = nn.ModuleList([
             Layer_scale_init_Block(
@@ -273,8 +274,7 @@ class VisionTransformer(nn.Module):
         if self.use_dense_prediction:
             self.head_dense = None
 
-
-# Use global average pooling
+        # Use global average pooling
         self.global_pool = global_pool
         if self.global_pool:
             self.fc_norm = norm_layer(embed_dim)
@@ -283,19 +283,16 @@ class VisionTransformer(nn.Module):
         trunc_normal_(self.pos_embed, std=.02)
         trunc_normal_(self.cls_token, std=.02)
 
-    def init_weights(self):
-        for m in self.modules():
-            if isinstance(m, nn.Linear):
-                trunc_normal_(m.weight, std=.02)
-                if isinstance(m, nn.Linear) and m.bias is not None:
-                    nn.init.constant_(m.bias, 0)
-            elif isinstance(m, nn.LayerNorm):
-                nn.init.constant_(m.bias, 0)
-                nn.init.constant_(m.weight, 1.0)
+        self.apply(self._init_weights)
 
-    @torch.jit.ignore
-    def no_weight_decay(self):
-        return {'pos_embed', 'cls_token'}
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=.02)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
 
     def get_classifier(self):
         return self.head
@@ -307,68 +304,13 @@ class VisionTransformer(nn.Module):
         self.num_classes = num_classes
         self.head = nn.Linear(self.embed_dim, num_classes) if num_classes > 0 else nn.Identity()
 
-
-
-    # def forward(self, x):
-    #     # convert to list
-    #     if not isinstance(x, list):
-    #         x = [x]
-    #     # Perform forward pass separately on each resolution input.
-    #     # The inputs corresponding to a single resolution are clubbed and single
-    #     # forward is run on the same resolution inputs. Hence we do several
-    #     # forward passes = number of different resolutions used. We then
-    #     # concatenate all the output features.
-    #     idx_crops = torch.cumsum(
-    #         torch.unique_consecutive(
-    #             torch.tensor([inp.shape[-1] for inp in x]),
-    #             return_counts=True,
-    #         )[1], 0)
-
-    #     if self.use_dense_prediction:
-    #         start_idx = 0
-
-    #         for end_idx in idx_crops:
-    #             _out_cls, _out_fea = self.forward_features(
-    #                 torch.cat(x[start_idx:end_idx]))
-    #             B, N, C = _out_fea.shape
-
-    #             if start_idx == 0:
-    #                 output_cls = _out_cls
-    #                 output_fea = _out_fea.reshape(B * N, C)
-    #                 npatch = [N]
-    #             else:
-    #                 output_cls = torch.cat((output_cls, _out_cls))
-    #                 output_fea = torch.cat(
-    #                     (output_fea, _out_fea.reshape(B * N, C)))
-    #                 npatch.append(N)
-    #             start_idx = end_idx
-
-    #         return [
-    #             self.head(output_cls),
-    #             self.head_dense(output_fea), output_fea, npatch
-    #         ]
-
-    #     else:
-    #         start_idx = 0
-    #         for end_idx in idx_crops:
-    #             _out = self.forward_features(torch.cat(x[start_idx:end_idx]))
-    #             # _out = self.forward_return_n_last_blocks(torch.cat(x[start_idx: end_idx]), 4, True)
-    #             if start_idx == 0:
-    #                 output = _out
-    #             else:
-    #                 output = torch.cat((output, _out))
-    #             start_idx = end_idx
-
-    #         # print(f'output[0] {output[0].shape}')
-    #         # Run the head forward on the concatenated features.
-    #         return [self.head(output)]
-
     def forward(self, x):
 
         x = self.forward_features(x)
         
-        # if self.dropout_rate:
-        #     x = F.dropout(x, p=float(self.dropout_rate), training=self.training)
+        if self.dropout_rate:
+            x = F.dropout(x, p=float(self.dropout_rate), training=self.training)
+
         x = self.head(x)
         
         return [x]
@@ -378,13 +320,9 @@ class VisionTransformer(nn.Module):
         x = self.patch_embed(x)
 
         cls_tokens = self.cls_token.expand(B, -1, -1)
-        # x = torch.cat((cls_tokens, x), dim=1)
-        # pos_embed = self.interpolate_pos_encoding(x, self.pos_embed)
-        # pos_embed = self.pos_embed
+
         x = x + self.pos_embed
         x = torch.cat((cls_tokens, x), dim=1)
-
-        x = self.pos_drop(x)
 
         for blk in self.blocks:
             x = blk(x)
@@ -399,123 +337,6 @@ class VisionTransformer(nn.Module):
                 return self.fc_norm(x)
             else:
                 return x[:, 0]
-
-    # def forward_feature_maps(self, x):
-    #     B = x.shape[0]
-    #     x = self.patch_embed(x)
-
-    #     cls_tokens = self.cls_token.expand(B, -1, -1)
-    #     x = torch.cat((cls_tokens, x), dim=1)
-    #     pos_embed = self.interpolate_pos_encoding(x, self.pos_embed)
-    #     x = x + pos_embed
-    #     x = self.pos_drop(x)
-
-    #     for blk in self.blocks:
-    #         x = blk(x)
-    #     if self.norm is not None:
-    #         x = self.norm(x)
-
-    #     return x
-
-    # def interpolate_pos_encoding(self, x, pos_embed):
-    #     npatch = x.shape[1] - 1
-    #     N = pos_embed.shape[1] - 1
-    #     if npatch == N:
-    #         return pos_embed
-    #     class_emb = pos_embed[:, 0]
-    #     pos_embed = pos_embed[:, 1:]
-    #     dim = x.shape[-1]
-    #     pos_embed = nn.functional.interpolate(
-    #         pos_embed.reshape(1, int(math.sqrt(N)), int(math.sqrt(N)),
-    #                           dim).permute(0, 3, 1, 2),
-    #         scale_factor=math.sqrt(npatch / N),
-    #         mode='bicubic',
-    #     )
-    #     pos_embed = pos_embed.permute(0, 2, 3, 1).view(1, -1, dim)
-    #     return torch.cat((class_emb.unsqueeze(0), pos_embed), dim=1)
-
-    # def forward_selfattention(self, x, n=1):
-    #     # n=1 return the last layer attn map; otherwise return attn maps in all layers
-
-    #     B, nc, w, h = x.shape
-    #     N = self.pos_embed.shape[1] - 1
-    #     x = self.patch_embed(x)
-
-    #     # interpolate patch embeddings
-    #     dim = x.shape[-1]
-    #     w0 = w // self.patch_embed.patch_size
-    #     h0 = h // self.patch_embed.patch_size
-    #     class_pos_embed = self.pos_embed[:, 0]
-    #     patch_pos_embed = self.pos_embed[:, 1:]
-    #     patch_pos_embed = nn.functional.interpolate(
-    #         patch_pos_embed.reshape(1, int(math.sqrt(N)), int(math.sqrt(N)),
-    #                                 dim).permute(0, 3, 1, 2),
-    #         scale_factor=(w0 / math.sqrt(N), h0 / math.sqrt(N)),
-    #         mode='bicubic',
-    #     )
-    #     if w0 != patch_pos_embed.shape[-2]:
-    #         helper = torch.zeros(h0)[None, None, None, :].repeat(
-    #             1, dim, w0 - patch_pos_embed.shape[-2], 1).to(x.device)
-    #         patch_pos_embed = torch.cat((patch_pos_embed, helper), dim=-2)
-    #     if h0 != patch_pos_embed.shape[-1]:
-    #         helper = torch.zeros(w0)[None, None, :, None].repeat(
-    #             1, dim, 1, h0 - patch_pos_embed.shape[-1]).to(x.device)
-    #         pos_embed = torch.cat((patch_pos_embed, helper), dim=-1)
-    #     patch_pos_embed = patch_pos_embed.permute(0, 2, 3, 1).view(1, -1, dim)
-    #     pos_embed = torch.cat((class_pos_embed.unsqueeze(0), patch_pos_embed),
-    #                           dim=1)
-
-    #     cls_tokens = self.cls_token.expand(B, -1, -1)
-    #     x = torch.cat((cls_tokens, x), dim=1)
-    #     x = x + pos_embed
-    #     x = self.pos_drop(x)
-
-    #     if n == 1:
-    #         return self.forward_last_selfattention(x)
-    #     else:
-    #         return self.forward_all_selfattention(x)
-
-    # def forward_last_selfattention(self, x):
-    #     for i, blk in enumerate(self.blocks):
-    #         if i < len(self.blocks) - 1:
-    #             x = blk(x)
-    #         else:
-    #             return blk(x, return_attention=True)
-
-    # def forward_all_selfattention(self, x):
-    #     attn_out = []
-    #     for i, blk in enumerate(self.blocks):
-    #         x, attn = blk.forward_fea_and_attn(x)
-    #         attn_out.append(attn)
-
-    #     return attn_out
-
-    # def forward_return_n_last_blocks(self,
-    #                                  x,
-    #                                  n=1,
-    #                                  return_patch_avgpool=False,
-    #                                  depths=[]):
-    #     B = x.shape[0]
-    #     x = self.patch_embed(x)
-
-    #     cls_tokens = self.cls_token.expand(B, -1, -1)
-    #     x = torch.cat((cls_tokens, x), dim=1)
-    #     pos_embed = self.interpolate_pos_encoding(x, self.pos_embed)
-    #     x = x + pos_embed
-    #     x = self.pos_drop(x)
-
-    #     # we will return the [CLS] tokens from the `n` last blocks
-    #     output = []
-    #     for i, blk in enumerate(self.blocks):
-    #         x = blk(x)
-    #         if len(self.blocks) - i <= n:
-    #             output.append(self.norm(x)[:, 0])
-    #     if return_patch_avgpool:
-    #         x = self.norm(x)
-    #         # In addition to the [CLS] tokens from the `n` last blocks, we also return
-    #         # the patch tokens from the last block. This is useful for linear eval.
-    #         output.append(torch.mean(x[:, 1:], dim=1))
-    #     return torch.cat(output, dim=-1)
 
 
 def deitiii_tiny_p16(patch_size=16, **kwargs):
