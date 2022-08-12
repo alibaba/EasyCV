072803cd10a7ca669767a5b5c4e772d3c7d9ed91
tuofeilunhifi <18292877852@163.com>
2022-08-12 19:32:03 +0800

    add dino_swinl_config

--- configs/detection/dino/dino_5scale_swinl_24e_coco.py
+++ configs/detection/dino/dino_5scale_swinl_24e_coco.py
@@ -1,5 +1,5 @@
 _base_ = [
-    './dino_4scale_swinl.py', '../_base_/dataset/autoaug_coco_detection.py',
+    './dino_5scale_swinl.py', '../_base_/dataset/autoaug_coco_detection.py',
     './dino_schedule_1x.py', 'configs/base.py'
 ]
 
