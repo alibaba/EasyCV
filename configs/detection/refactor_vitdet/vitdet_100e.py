a41514f834dccdaeb806b8d9018afc2eb9eca4c7
tuofeilunhifi <18292877852@163.com>
2022-08-29 17:08:55 +0800

    update vitdet_scheduler

--- configs/detection/refactor_vitdet/vitdet_100e.py
+++ configs/detection/refactor_vitdet/vitdet_100e.py
@@ -14,9 +14,7 @@ checkpoint_config = dict(interval=10)
 # optimizer
 paramwise_options = {
     'norm': dict(weight_decay=0.),
-    'bias': dict(weight_decay=0.),
     'pos_embed': dict(weight_decay=0.),
-    'cls_token': dict(weight_decay=0.)
 }
 optimizer = dict(
     type='AdamW',
@@ -24,13 +22,13 @@ optimizer = dict(
     betas=(0.9, 0.999),
     weight_decay=0.1,
     paramwise_options=paramwise_options)
-optimizer_config = dict(grad_clip=None, loss_scale=512.)
+optimizer_config = dict(grad_clip=None)
 # learning policy
 lr_config = dict(
     policy='step',
     warmup='linear',
     warmup_iters=250,
-    warmup_ratio=0.067,
+    warmup_ratio=0.001,
     step=[88, 96])
 total_epochs = 100
 
