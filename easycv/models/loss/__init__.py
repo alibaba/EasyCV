51cf2a3268d1d561575a08a2d2051f61114e3643
tuofeilunhifi <18292877852@163.com>
2022-08-30 19:46:45 +0800

    bugfix

--- easycv/models/loss/__init__.py
+++ easycv/models/loss/__init__.py
@@ -4,4 +4,5 @@ from .focal_loss import FocalLoss, VarifocalLoss
 from .iou_loss import GIoULoss, IoULoss, YOLOX_IOULoss
 from .mse_loss import JointsMSELoss
 from .pytorch_metric_learning import *
-from .set_criterion import CDNCriterion, DNCriterion, SetCriterion
+from .set_criterion import (CDNCriterion, DNCriterion, HungarianMatcher,
+                            SetCriterion)
