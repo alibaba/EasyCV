440df70d62bae8e97e989ed810efc70adea632d2
zzoneee <2829473728@qq.com>
2022-08-30 14:10:51 +0800

    DeiT III: config

--- configs/classification/imagenet/vit/imagenet_deitiii_small_patch16_224_jpg.py
+++ configs/classification/imagenet/vit/imagenet_deitiii_small_patch16_224_jpg.py
@@ -1,17 +1,16 @@
-_base_ = './deitiii_large_patch16_192.py'
+_base_ = './deitiii_small_patch16_224.py'
 # model settings
 model = dict(
     type='Classification',
     backbone=dict(
         type='DeiTIII',
-        img_size=[192],
+        img_size=[224],
         num_classes=1000,
         patch_size=16,
-        embed_dim=1024,
-        depth=24,
-        num_heads=16,
+        embed_dim=384,
+        depth=12,
+        num_heads=6,
         mlp_ratio=4,
         qkv_bias=True,
         drop_rate=0.,
-        drop_path_rate=0.45,
-    ))
+        drop_path_rate=0.05))
