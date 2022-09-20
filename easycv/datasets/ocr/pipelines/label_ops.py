48244acf18efe0279dcb5b6491442cb2b63bb5e4
yanhaiqiang.yhq <yanhaiqiang.yhq@alibaba-inc.com>
2022-09-20 16:18:45 +0800

    remove __future__

--- easycv/datasets/ocr/pipelines/label_ops.py
+++ easycv/datasets/ocr/pipelines/label_ops.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from __future__ import (absolute_import, division, print_function,
-                        unicode_literals)
 import copy
 import math
 import os.path as osp
