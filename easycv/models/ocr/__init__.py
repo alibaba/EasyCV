c46122c5c736593c354a0aa5a98572c50be645fd
yanhaiqiang.yhq <yanhaiqiang.yhq@alibaba-inc.com>
2022-09-20 16:53:23 +0800

    use explicit import

--- easycv/models/ocr/__init__.py
+++ easycv/models/ocr/__init__.py
@@ -1,6 +1,6 @@
 # Copyright (c) Alibaba, Inc. and its affiliates.
-from .cls import *
-from .det import *
-from .heads import *
-from .necks import *
-from .rec import *
+from .cls import TextClassifier
+from .det import DBNet
+from .heads import CTCHead, DBHead
+from .necks import DBFPN, LKPAN, RSEFPN, SequenceEncoder
+from .rec import OCRRecNet
