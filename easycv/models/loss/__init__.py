5ed2a80bd308c5c60505528e4211ac14e419819c
yanhaiqiang.yhq <yanhaiqiang.yhq@alibaba-inc.com>
2022-09-21 14:25:47 +0800

    build loss by builder

--- easycv/models/loss/__init__.py
+++ easycv/models/loss/__init__.py
@@ -5,7 +5,7 @@ from .face_keypoint_loss import FacePoseLoss, WingLossWithPose
 from .focal_loss import FocalLoss, VarifocalLoss
 from .iou_loss import GIoULoss, IoULoss, YOLOX_IOULoss
 from .mse_loss import JointsMSELoss
+from .ocr_rec_multi_loss import MultiLoss
 from .pytorch_metric_learning import *
-from .rec_multi_loss import MultiLoss
 from .set_criterion import (CDNCriterion, DNCriterion, HungarianMatcher,
                             SetCriterion)
