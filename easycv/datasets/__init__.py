2a92f67ac2cd192792f3a3a172f14a0bde636d48
jiangnana.jnn <jiangnana.jnn@alibaba-inc.com>
2022-10-12 13:29:13 +0800

    fix code check

--- easycv/datasets/__init__.py
+++ easycv/datasets/__init__.py
@@ -2,8 +2,8 @@
 # isort:skip_file
 from easycv.utils.import_utils import check_numpy
 check_numpy()
-from . import (classification, detection, face, ocr, pose, segmentation, selfsup,
-               shared)
+from . import (classification, detection, face, ocr, pose, segmentation,
+               selfsup, shared)
 from .builder import build_dali_dataset, build_dataset
 from .loader import DistributedGroupSampler, GroupSampler, build_dataloader
 from .registry import DATASETS
