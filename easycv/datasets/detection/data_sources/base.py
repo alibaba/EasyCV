fe992f4bdfc2f383d78a19bf6e2cc3e19a225078
lxs01517514 <lxs01517514@alibaba-inc.com>
2022-10-26 18:47:45 +0800

    modify something

--- easycv/datasets/detection/data_sources/base.py
+++ easycv/datasets/detection/data_sources/base.py
@@ -4,9 +4,9 @@ import functools
 import logging
 from abc import abstractmethod
 from multiprocessing import Pool, cpu_count
+
 import numpy as np
 from mmcv.runner.dist_utils import get_dist_info
-
 from tqdm import tqdm
 
 from easycv.file.image import load_image
