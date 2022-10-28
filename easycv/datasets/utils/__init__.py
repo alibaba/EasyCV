df0adc7f5c9dd50f4380a91ab576b286abe6dfd7
lxs01517514 <lxs01517514@alibaba-inc.com>
2022-10-28 11:11:29 +0800

    modify

--- easycv/datasets/utils/__init__.py
+++ easycv/datasets/utils/__init__.py
@@ -1,3 +1,4 @@
 from .tfrecord_util import download_tfrecord, get_path_and_index
 from .transform_util import replace_ImageToTensor
 from .type_util import is_dali_dataset_type
+from .download_util import DownLoadDataFile
