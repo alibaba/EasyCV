9c1e603566f1db4823785d4df8149364c25d0009
lxs01517514 <lxs01517514@alibaba-inc.com>
2022-11-22 14:47:09 +0800

    add Seg of coco data source

--- easycv/datasets/segmentation/data_sources/__init__.py
+++ easycv/datasets/segmentation/data_sources/__init__.py
@@ -1,7 +1,9 @@
 # Copyright (c) Alibaba, Inc. and its affiliates.
+from .coco import SegSourceCoco, SegSourceCoco2017
 from .raw import SegSourceRaw
 from .voc import SegSourceVoc2007, SegSourceVoc2010, SegSourceVoc2012
 
 __all__ = [
-    'SegSourceRaw', 'SegSourceVoc2010', 'SegSourceVoc2007', 'SegSourceVoc2012'
+    'SegSourceRaw', 'SegSourceVoc2010', 'SegSourceVoc2007', 'SegSourceVoc2012',
+    'SegSourceCoco', 'SegSourceCoco2017'
 ]
