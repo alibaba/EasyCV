ef11bf05b8ed2eaf28c5995f554984ca68eef5b0
伝迹 <yunji.cjy@alibaba-inc.com>
2023-02-01 14:21:28 +0800

    clear

--- easycv/hooks/export_hook.py
+++ easycv/hooks/export_hook.py
@@ -1,7 +1,6 @@
 # Copyright (c) Alibaba, Inc. and its affiliates.
 import os
 import shutil
-import tarfile
 
 import requests
 from mmcv.runner import Hook
@@ -9,39 +8,6 @@ from mmcv.runner.dist_utils import master_only
 
 from easycv.utils.config_tools import validate_export_config
 from .registry import HOOKS
-
-
-def make_targz(output_filename, source_dir):
-    """
-    一次性打包目录为tar.gz
-    :param output_filename: 压缩文件名
-    :param source_dir: 需要打包的目录
-    :return: bool
-    """
-    try:
-        with tarfile.open(output_filename, 'w:gz') as tar:
-            tar.add(source_dir, arcname=os.path.basename(source_dir))
-
-        return True
-    except Exception as e:
-        print(e)
-        return False
-
-
-def untar(fname, dirs):
-    """
-    解压tar.gz文件
-    :param fname: 压缩文件名
-    :param dirs: 解压后的存放路径
-    :return: bool
-    """
-    try:
-        t = tarfile.open(fname)
-        t.extractall(path=dirs)
-        return True
-    except Exception as e:
-        print(e)
-        return False
 
 
 @HOOKS.register_module
