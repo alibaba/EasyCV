c777a042979905c75eb69bfd9c873ad46848176f
伝迹 <yunji.cjy@alibaba-inc.com>
2023-02-13 10:18:04 +0800

    bugfix

--- easycv/core/evaluation/classification_eval.py
+++ easycv/core/evaluation/classification_eval.py
@@ -66,12 +66,13 @@ class ClsEvaluator(Evaluator):
                 'Inconsistent length for results and labels, {} vs {}'.format(
                 scores.size(0), target.size(0))
             num = scores.size(0)
-            _, pred = scores.topk(
-                max(self._topk), dim=1, largest=True, sorted=True)
 
             # Avoid topk values greater than the number of categories
             self._topk = np.array(list(self._topk))
             self._topk = np.clip(self._topk, 1, scores.shape[-1])
+
+            _, pred = scores.topk(
+                max(self._topk), dim=1, largest=True, sorted=True)
 
             pred = pred.t()
             correct = pred.eq(target.view(1, -1).expand_as(pred))  # KxN
