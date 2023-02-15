371244e323f36aefa098bb1c22695908a7a9fd2b
yanhaiqiang.yhq <yanhaiqiang.yhq@alibaba-inc.com>
2023-02-15 18:14:17 +0800

    fix ut bug

--- easycv/datasets/segmentation/data_sources/cityscapes.py
+++ easycv/datasets/segmentation/data_sources/cityscapes.py
@@ -105,6 +105,8 @@ class SegSourceCityscapes(SegSourceRaw):
 
         self.label_files = []
         for img_path in self.img_files:
+            if self.img_root[-1] != '/':
+                self.img_root += '/'
             img_name = img_path.replace(self.img_root,
                                         '')[:-len(self.img_suffix[0])]
             find_label_path = False
