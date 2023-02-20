8f49e1fa4f9590881ac8a7b1a27f95d4fe513495
Cathy0908 <30484308+Cathy0908@users.noreply.github.com>
2023-02-20 15:29:28 +0800

    fix WholeBodyKeyPointEvaluator (#287)

--- easycv/datasets/pose/wholebody_topdown_coco_dataset.py
+++ easycv/datasets/pose/wholebody_topdown_coco_dataset.py
@@ -28,26 +28,20 @@ class WholeBodyCocoTopDownDataset(RawDataset):
         if len(evaluators) > 1 or not isinstance(evaluators[0],
                                                  WholeBodyKeyPointEvaluator):
             raise ValueError(
-                'HandCocoWholeBodyDataset only support one `WholeBodyKeyPointEvaluator` now, '
+                'WholeBodyCocoTopDownDataset only support one `WholeBodyKeyPointEvaluator` now, '
                 'but get %s' % evaluators)
-        evaluator = evaluators[0]
 
-        image_ids = outputs['image_ids']
-        preds = outputs['preds']
-        boxes = outputs['boxes']
-        bbox_ids = outputs['bbox_ids']
+        evaluator_args = {
+            'num_joints': self.data_source.ann_info['num_joints'],
+            'sigmas': self.data_source.sigmas,
+            'class2id': self.data_source._class_to_ind
+        }
+        eval_result = {}
+        for evaluator in evaluators:
+            eval_result.update(
+                evaluator.evaluate(
+                    prediction_dict=outputs,
+                    groundtruth_dict=self.data_source.coco.dataset,
+                    **evaluator_args))
 
-        kpts = []
-        for i, image_id in enumerate(image_ids):
-            kpts.append({
-                'keypoints': preds[i],
-                'center': boxes[i][0:2],
-                'scale': boxes[i][2:4],
-                'area': boxes[i][4],
-                'score': boxes[i][5],
-                'image_id': image_id,
-                'bbox_id': bbox_ids[i]
-            })
-        kpts = self._sort_and_unique_bboxes(kpts)
-        eval_res = evaluator.evaluate(kpts, self.data_source.db)
-        return eval_res
+        return eval_result
