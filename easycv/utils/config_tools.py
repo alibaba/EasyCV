5852768d58ecabee22933511cb7a21f1b2b9bde8
lxs01517514 <lxs01517514@alibaba-inc.com>
2023-03-08 13:28:12 +0800

    udpate

--- easycv/utils/config_tools.py
+++ easycv/utils/config_tools.py
@@ -533,8 +533,9 @@ CONFIG_TEMPLATE_ZOO = {
 
     # segmentation
     'FCN_SEG': 'configs/segmentation/fcn/fcn_r50-d8_512x512_8xb4_60e_voc12.py',
-    'UPNET_SEG':
+    'UPERNET_SEG':
     'configs/segmentation/upernet/upernet_r50_512x512_8xb4_60e_voc12.py',
+    'SEGFORMER_SEG': 'configs/segmentation/segformer/segformer_b5_coco.py',
 
     # ssl
     'MOCO_R50_TFRECORD': 'configs/config_templates/moco_r50_tfrecord.py',
